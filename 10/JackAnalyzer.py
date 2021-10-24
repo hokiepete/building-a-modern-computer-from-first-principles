bba0d437b10ba5b5f6861067b3a0ba4ac14ef447
Peter <pnolan1986@gmail.com>
2021-10-23 21:41:56 -0400

    updates

--- 10/JackAnalyzer.py
+++ 10/JackAnalyzer.py
@@ -1,26 +1,30 @@
 import os
 from JackTonenizer import JackTokenizer
 from CompilationEngine import CompilationEngine
+import argparse
 
-srcpath = 'ExpressionLessSquare'
-# srcpath = 'C:\\Users\\pnola\\OneDrive\\Desktop\\nand2tetris\\projects\\09\\my_snake_game'
-if os.path.isdir( srcpath):
+parser = argparse.ArgumentParser()
+parser.add_argument('p', help='path to the file to translate')
+args = parser.parse_args()
+srcpath = args.p
+
+if os.path.isdir(srcpath):
     # read and parse the system file
-    # with open(srcpath + '\\Sys.vm', 'r') as file:
+    # with open(srcpath + '/Sys.vm', 'r') as file:
     #     text = file.read()
     # get all the files in the directory minus the system file
     # and parse the files
     files = os.listdir(srcpath)
     for file in files:
         if file.endswith('.jack'):
-            with open(srcpath + f'\\{file}', 'r') as f:
+            with open(srcpath + f'/{file}', 'r') as f:
                 text = f.read()
             tokenizer = JackTokenizer(text)
             try:
                 compiler = CompilationEngine(tokenizer.tagged_tokens)
             except ValueError as e:
                 raise ValueError(f"Error in file {file}. {e}")
-            destfile = f'{srcpath}\\{file.replace(".jack", ".xml")}'
+            destfile = f'{srcpath}/{file.replace(".jack", ".xml")}'
             with open(destfile, 'w') as f:
                 f.write('\n'.join(compiler.code)+'\n')
 else:
--- 11/JackCompiler.py
+++ 11/JackCompiler.py
@@ -1,38 +1,44 @@
 import os
 from JackTonenizer import JackTokenizer
 from CompilationEngine import CompilationEngine
+import argparse
 
-# srcpath = '11\\Seven'
-# srcpath = '11\\ConvertToBin'
-# srcpath = '11\\Square'
-srcs = ['11\\Seven', '11\\ConvertToBin', '11\\Square', '11\\Average', '11\\Pong', '11\\ComplexArrays']
-# srcs = ['11\\Pong\\Ball.jack']
-for srcpath in srcs:
-    print(srcpath)
-    if os.path.isdir(srcpath):
-        # read and parse the system file
-        # with open(srcpath + '\\Sys.vm', 'r') as file:
-        #     text = file.read()
-        # get all the files in the directory minus the system file
-        # and parse the files
-        files = os.listdir(srcpath)
-        for file in files:
-            if file.endswith('.jack'):
-                with open(srcpath + f'\\{file}', 'r') as f:
-                    text = f.read()
-                tokenizer = JackTokenizer(text)
-                try:
-                    compiler = CompilationEngine(tokenizer.tagged_tokens)
-                except ValueError as e:
-                    raise ValueError(f"Error in file {file}. {e}")
-                destfile = f'{srcpath}\\{file.replace(".jack", ".vm")}'
-                with open(destfile, 'w') as f:
-                    f.write('\n'.join(compiler.code)+'\n')
-    else:
-        with open(srcpath, 'r') as file:
-            text = file.read()
-        tokenizer = JackTokenizer(text)
-        compiler = CompilationEngine(tokenizer.tagged_tokens)
-        destfile = f'{srcpath.replace(".jack", ".vm")}'
-        with open(destfile, 'w') as f:
-            f.write('\n'.join(compiler.code)+'\n')
+parser = argparse.ArgumentParser()
+parser.add_argument('p', help='path to the file to translate')
+args = parser.parse_args()
+srcpath = args.p
+
+# srcpath = '11/Seven'
+# srcpath = '11/ConvertToBin'
+# srcpath = '11/Square'
+# srcs = ['11/Seven', '11/ConvertToBin', '11/Square', '11/Average', '11/Pong', '11/ComplexArrays']
+# srcs = ['11/Pong/Ball.jack']
+# srcs = ['12/MathTest']
+
+if os.path.isdir(srcpath):
+    # read and parse the system file
+    # with open(srcpath + '/Sys.vm', 'r') as file:
+    #     text = file.read()
+    # get all the files in the directory minus the system file
+    # and parse the files
+    files = os.listdir(srcpath)
+    for file in files:
+        if file.endswith('.jack'):
+            with open(srcpath + f'/{file}', 'r') as f:
+                text = f.read()
+            tokenizer = JackTokenizer(text)
+            try:
+                compiler = CompilationEngine(tokenizer.tagged_tokens)
+            except ValueError as e:
+                raise ValueError(f"Error in file {file}. {e}")
+            destfile = f'{srcpath}/{file.replace(".jack", ".vm")}'
+            with open(destfile, 'w') as f:
+                f.write('\n'.join(compiler.code)+'\n')
+else:
+    with open(srcpath, 'r') as file:
+        text = file.read()
+    tokenizer = JackTokenizer(text)
+    compiler = CompilationEngine(tokenizer.tagged_tokens)
+    destfile = f'{srcpath.replace(".jack", ".vm")}'
+    with open(destfile, 'w') as f:
+        f.write('\n'.join(compiler.code)+'\n')
